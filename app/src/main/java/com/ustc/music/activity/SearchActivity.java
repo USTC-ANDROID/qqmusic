3329a54b7be96d6117d16fffb477f69b8d2f2a07
WilburYao <xiqv926@163.com>
2019-10-05 15:00:33 +0800

    实现搜索页面

--- app/src/main/java/com/ustc/music/activity/SearchActivity.java
+++ app/src/main/java/com/ustc/music/activity/SearchActivity.java
@@ -1,15 +1,54 @@
 package com.ustc.music.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
 
 import com.ustc.music.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private SearchView searchView;
+    private ImageView goback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        initView();
     }
+
+    void initView() {
+        searchView = findViewById(R.id.qq_search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String queryStr = s.trim();
+                if (queryStr.isEmpty()) {
+                    return false;
+                } else {
+                    Intent intent = new Intent(SearchActivity.this, SearchResultActivity.class);
+                    intent.putExtra("queryKeyWord", queryStr);
+                    startActivity(intent);
+                    return true;
+                }
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        goback = findViewById(R.id.goback);
+        goback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
 }
