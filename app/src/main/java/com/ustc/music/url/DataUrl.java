339e10b168cfdceb3fdbe51de3152c48e1a34a32
WilburYao <xiqv926@163.com>
2019-10-06 10:57:35 +0800

    add search url

--- app/src/main/java/com/ustc/music/url/DataUrl.java
+++ app/src/main/java/com/ustc/music/url/DataUrl.java
@@ -7,4 +7,6 @@ public interface DataUrl {
      */
     String Banner = "https://u.y.qq.com/cgi-bin/musicu.fcg?data={%22focus%22:%20{%22module%22:%20%22QQMusic.MusichallServer%22,%22method%22:%20%22GetFocus%22,%22param%22:%20{}}}";
 
+    String searchUrl = "http://api.qq.jsososo.com/search?";
+
 }
