375e8ae776133cd41e007d18af6f7d4edddcd33b
WilburYao <xiqv926@163.com>
2019-10-08 13:41:26 +0800

    下拉刷新不显示

--- app/src/main/java/com/ustc/music/fragment/SearchSongFragment.java
+++ app/src/main/java/com/ustc/music/fragment/SearchSongFragment.java
@@ -56,13 +56,13 @@ public class SearchSongFragment extends Fragment implements PullLoadMoreRecycler
         //代码设置scrollbar无效？未解决！
         mRecyclerView.setVerticalScrollBarEnabled(true);
         //设置下拉刷新是否可见
-        //mPullLoadMoreRecyclerView.setRefreshing(true);
+//        mPullLoadMoreRecyclerView.setRefreshing(true);
         //设置是否可以下拉刷新
-        //mPullLoadMoreRecyclerView.setPullRefreshEnable(true);
+//        mPullLoadMoreRecyclerView.setPullRefreshEnable(false);
         //设置是否可以上拉刷新
         //mPullLoadMoreRecyclerView.setPushRefreshEnable(false);
         //显示下拉刷新
-        mPullLoadMoreRecyclerView.setRefreshing(true);
+        mPullLoadMoreRecyclerView.setRefreshing(false);
         //设置上拉刷新文字
         mPullLoadMoreRecyclerView.setFooterViewText("正在加载更多");
         //设置上拉刷新文字颜色
